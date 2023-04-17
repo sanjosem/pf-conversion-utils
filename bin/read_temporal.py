a09999fd1f46027f509450313195b7c9f6261d98
Marlene Sanjose <marlene.sanjose@etsmtl.ca>
2023-04-16 22:38:51 -0400

    Export probe locations

--- bin/read_temporal.py
+++ bin/read_temporal.py
@@ -7,6 +7,7 @@ import sys
 import os.path
 from os import makedirs
 from glob import glob
+from pandas import DataFrame
 
 import pftools.pnc_conv
 
@@ -41,9 +42,12 @@ liste = glob('{0:s}/{1:s}*.{2:s}'.format(uinfo['directory'],uinfo['probe_name'],
 if len(liste) == 0:
     sys.exit('No probe file \'{1:s}*.{2:s}\' found in directory {0:s}'.format(uinfo['directory'],uinfo['probe_name'],uinfo['file_type']))
 
+probe_info = {'name': [], 'xc': [], 'yc': [], 'zc': []}
+
 for fl in liste:
     probe_file = os.path.basename(fl)
     casename = probe_file.replace('.psnc','').replace('.pfnc','')
+    probe_info['name'].append(casename)
 
     print('*** Processing {0:s} ***'.format(probe_file))
     
@@ -53,6 +57,15 @@ for fl in liste:
         pncConv = pftools.pnc_conv.pncConversion(fl)
                 
     pncConv.extract_probe()
-    pncConv.get_probe_location()
+    center = pncConv.get_probe_location()
+    for idim,coor in enumerate(['xc','yc','zc']):
+        probe_info[coor].append(center[idim])
+
     pncConv.export_temporal_data(casename,uinfo['output_directory'],delimiter=uinfo['delimiter'])
     
+DataFrame(probe_info).to_csv(os.path.join(uinfo['output_directory'],'probe_info.csv'))
+if verbose in uinfo.keys():
+    if uinfo['verbose']:
+        print(DataFrame(probe_info))
+else: 
+    print('Probe location information is written in probe_info.csv')
